30360e008ef79caf9cb9525aa91aa7d581769948
Bhaveshanjana <bhaveshanjana58@gmail.com>
2025-04-17 09:35:44 +0530

    feat(admin): working on admin ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import Course from "./pages/Courses";
-import BuyCourse from "./pages/BuyCourse";
-import Purchased from "./pages/Purchased";
+import Course from "./pages/User/Courses";
+import BuyCourse from "./pages/User/BuyCourse";
+import Purchased from "./pages/User/Purchased";
 import { ToastContainer } from "react-toastify";
-import UpdateUser from "./pages/UpdateUser";
+import UpdateUser from "./pages/User/UpdateUser";
 import AuthUser from "./auth/UserProtectWrapper";
 
 function App() {
@@ -24,8 +24,22 @@ function App() {
             </AuthUser>
           }
         />
-        <Route path="/buy/:courseId" element={<BuyCourse />} />
-        <Route path="/Purchased" element={<Purchased />} />
+        <Route
+          path="/buy/:courseId"
+          element={
+            <AuthUser>
+              <BuyCourse />
+            </AuthUser>
+          }
+        />
+        <Route
+          path="/Purchased"
+          element={
+            <AuthUser>
+              <Purchased />
+            </AuthUser>
+          }
+        />
         <Route
           path="/update-user"
           element={
